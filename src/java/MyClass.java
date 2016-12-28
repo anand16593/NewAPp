6837b090732be573343ecb1d0336f98ab57a4b7f
anand16593 <anand16593@gmail.com>
2016-12-28 18:47:03 +0530

    first push

---
+++ src/java/MyClass.java
@@ -0,0 +1,12 @@
+package java;
+
+/**
+ * Created by AnandG on 28-12-2016.
+ */
+public class MyClass {
+
+    public static void main(String m)
+    {
+        System.out.println("adsd");
+    }
+}
